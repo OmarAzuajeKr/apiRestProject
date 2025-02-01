ad8e4c8ace06bae9d077992b803d4b20a660094f
Omar <omarazuaje2323@hotmail.com>
2025-01-31 22:24:38 -0400

    Crear funciones de create y update

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -11,11 +11,36 @@ class CategoriaController extends Controller
     {
         return response()->json(Categoria::all(), 200);
     }
-    public function getCategoriaById($id){
-      $categoria = categoria::find($id);
-      if(is_null($categoria)){
-        return response()->json(['message'=>'Categoria Not Found'], 404);
+
+    public function getCategoriaById($id)
+    {
+        $categoria = Categoria::find($id);
+        if (is_null($categoria)) {
+            return response()->json(['message' => 'Categoria Not Found'], 404);
+        }
+        return response()->json($categoria, 200);
+    }
+
+    public function insertCategoria(Request $request)
+    {
+        // Validar los datos de entrada
+        $validatedData = $request->validate([
+            'cat_nom' => 'required|string|max:255',
+            'cat_obs' => 'nullable|string|max:255',
+        ]);
+
+        // Crear una nueva categoría
+        $categoria = Categoria::create($validatedData);
+
+        // Devolver la respuesta con el código de estado 201
+        return response($categoria, 201);
+    }
+    public function updateCategoria(Request $request, $id){
+      $categoria = Categoria::find($id);
+      if (is_null($categoria)) {
+          return response()->json(['message' => 'Categoria Not Found'], 404);
       }
-      return response()->json($categoria::find($id), 200);
+      $categoria->update($request->all());
+      return response($categoria, 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('Categoria', [App\Http\Controllers\CategoriaController::class, 'getCategoria']);
-Route::get('Categoria/{id}', [App\Http\Controllers\CategoriaController::class, 'getCategoriaById']);
+Route::get('Categoria', [CategoriaController::class, 'getCategoria']);
+Route::get('Categoria/{id}', [CategoriaController::class, 'getCategoriaById']);
+Route::post('addCategoria', [CategoriaController::class, 'insertCategoria']);
+Route::put('updateCategoria/{id}', [CategoriaController::class, 'updateCategoria']);
